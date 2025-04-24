0375dfc0015e005c3e6a53cb14b0560a05cf1633
Wangui <elswangui@gmail.com>
2025-04-25 01:33:09 +0300

    Refactor Home component to display an overview of client data

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './index.css';
 
-function Home() {
-  return <h2>Welcome to Health Bridge</h2>;
+function HomePage() {
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Welcome to Health Bridge</h1>
+      <p className="home-description">
+        Your trusted system for managing clients and health programs.
+      </p>
+      <ul className="home-features">
+        <li> Register and manage client profiles</li>
+        <li> Enroll clients in health programs like Malaria, Diabetes & Hypertension</li>
+        <li> Search and update client records</li>
+        <li> View all registered health programs</li>
+      </ul>
+
+      <div className="home-links">
+        <Link className="home-button" to="/clients/new">Add New Client</Link>
+        <Link className="home-button" to="/programs"> Manage Health Programs</Link>
+        <Link className="home-button" to="/clients">View All Clients</Link>
+        <Link className="home-button" to="/clients"> Search Client</Link>
+      </div>
+    </div>
+  );
 }
 
-export default Home;
+export default HomePage;

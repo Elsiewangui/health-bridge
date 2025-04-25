d46966995309b1af8f1c1766bd08bb0fd218a1d1
Wangui <elswangui@gmail.com>
2025-04-25 18:29:16 +0300

    Update App.js to integrate login and protect routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route , useLocation} from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Home from './Home';
 import AddProgram from './AddProgram';
 import AddClient from './AddClient';
@@ -7,29 +7,32 @@ import AllClients from './AllClients';
 import ClientProfile from './ClientProfile';
 import EditClient from './EditClient';
 import NavBar from './NavBar'; 
+import Login from './Login'; 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { isAuthenticated } from "./Auth";
 
 function App() {
   const location = useLocation();
-
   const isHomePage = location.pathname === '/';
+  
 
   return (
     <div className="App">
       {/* Only show Navbar if NOT on homepage */}
-      {!isHomePage && <NavBar />}
+      {isAuthenticated() && !isHomePage && <NavBar />}
+
 
       {/* Routes */}
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/programs" element={<AddProgram />} />
         <Route path="/clients/new" element={<AddClient />} />
         <Route path="/clients" element={<AllClients />} />
         <Route path="/client/:id" element={<ClientProfile />} />
         <Route path="/clients/:id/edit" element={<EditClient />} />
-        
+        <Route path="/login" element={<Login />} /> {/* Add Login Route */}
       </Routes>
       <ToastContainer position="top-right" autoClose={3000} closeOnClick />
     </div>
